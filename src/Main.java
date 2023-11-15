5e080227c1f0ce784cb2f0214f0ce78c877917bc
Haricks <fabian.bacher@opoczta.pl>
2023-11-15 09:37:44 +0100

    drugi commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+//ostatnie dwa znaki, minimalnie 3 znaki
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj stringa");
+        String testowy = scanner.nextLine();
+        System.out.println(testowy);
 
+//       public static String ostatnieznaki(String testowy){
+
+
+        String custom = new String();
+        if (testowy.length() >= 3) {
+            testowy = testowy.substring(3);
+
+        }
+        // Nie zdążyłem ale dokończyłbym, wiem że substring inny i warunek ale czasu brakło
+        System.out.println(testowy);
     }
 }
